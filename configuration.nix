d7307e6f2a54db1c6aba4b80edb3fc76e1edb249
dp <gits@annul.io>
2025-04-17 12:10:29 +0530

    mod

--- configuration.nix
+++ configuration.nix
@@ -1,4 +1,3 @@
-# dbna
 # Edit this configuration file to define what should be installed on
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
@@ -128,3 +127,4 @@
   system.stateVersion = "24.11"; # Did you read the comment?
 
 }
+# dbna
